1926de036a8aa450745f25f5889436e4eb11f855
AMills19 <144487240+AMills19@users.noreply.github.com>
2023-11-01 15:20:01 -0400

    Add files via upload

---
+++ Chi-square-test.R
@@ -0,0 +1,16 @@
+date: Nov 1, 2023; author: Alexander Mills; purpose: Perform the Chi-square analysis
+
+  data_frame <- read.csv("https://goo.gl/j6lRXD")
+
+
+table(data_frame$treatment, data_frame$improvement)
+
+
+ chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+ 
+ 
+Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
+
